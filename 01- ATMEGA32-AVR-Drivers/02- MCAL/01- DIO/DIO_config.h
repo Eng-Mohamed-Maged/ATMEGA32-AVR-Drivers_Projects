3eaa931135cc9b31923aedc58e6a171d832ce420
Eng-Mohamed-Maged <mohamed.maged.khalil1@gmail.com>
2023-10-21 11:16:39 +0300

    Initial Creation for [DIO] Drivers

---
+++ 01- ATMEGA32-AVR-Drivers/02- MCAL/01- DIO/DIO_config.h
@@ -0,0 +1,14 @@
+/****************************************************************************/
+/* Author      : Mohamed Maged                                              */
+/* Version     : V01                                                        */
+/* Date        : 20 October 2023                                            */
+/* Description : DIO_config.h                                               */
+/****************************************************************************/
+
+#ifndef DIO_CONFIG_H_
+#define DIO_CONFIG_H_
+
+#define DIO_BASE_ADDRESS           0x30
+
+
+#endif /* DIO_CONFIG_H_ */
